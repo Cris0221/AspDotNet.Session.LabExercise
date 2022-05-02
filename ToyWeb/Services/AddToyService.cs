e05c04362aa703c920951861e67d5c5c56ed68ec
Cris0221 <cristobal.fragata@pointwest.com.ph>
2022-05-02 09:56:46 +0800

    first commit

---
+++ ToyWeb/Services/AddToyService.cs
@@ -0,0 +1,24 @@
+﻿//using System.Collections.Generic;
+//using System.Linq;
+//using ToyWeb.Models;
+//using ToyData.Models;
+
+//namespace ToyWeb.Services
+//{
+//    public class AddToyService
+//    {
+//        private List<Item> products;
+
+
+//        public List<Item> FindAll()
+//        {
+//            return this.products;
+//        }
+
+//        public Item Find(string id)
+//        {
+//            return this.products.Single(p => p.Id.Equals(id));
+//        }
+//    }
+//}
+
